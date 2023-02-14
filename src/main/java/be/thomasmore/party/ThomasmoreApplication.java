5b582ce61c06c43ef34ec90bb11b6d3992ad5cee
tosjDP <tosj@hotmail.be>
2023-02-14 12:00:48 +0100

    first commit

---
+++ src/main/java/be/thomasmore/party/ThomasmoreApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.party;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThomasmoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThomasmoreApplication.class, args);
+    }
+
+}
---
+++ src/main/java/be/thomasmore/party/controller/HomeController.java
@@ -0,0 +1,18 @@
+package be.thomasmore.party.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping({"/","/home"})
+    public String home() {
+        return "home";
+    }
+
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+}
