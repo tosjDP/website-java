bbbdb069949b243c0c1199bbf92547c6dd3755b0
tosjDP <tosj@hotmail.be>
2023-02-14 12:25:07 +0100

    nummer laten tonen

--- src/main/java/be/thomasmore/party/controller/HomeController.java
+++ src/main/java/be/thomasmore/party/controller/HomeController.java
@@ -1,12 +1,15 @@
 package be.thomasmore.party.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping({"/","/home"})
-    public String home() {
+    public String home(Model model) {
+        final int calculatedValue = 45 * 34;
+        model.addAttribute("calculatedValue",calculatedValue);
         return "home";
     }
 
