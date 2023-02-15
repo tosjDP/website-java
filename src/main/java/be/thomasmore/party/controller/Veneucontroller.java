aef561cd78cab4fc84f19a3decc7bd96846e0b07
tosjDP <tosj@hotmail.be>
2023-02-15 12:17:45 +0100

    venue class + entity en h2 database ingesteld

--- src/main/java/be/thomasmore/party/controller/Veneucontroller.java
+++ src/main/java/be/thomasmore/party/controller/Veneucontroller.java
@@ -9,8 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class Veneucontroller {
     @GetMapping({ "/Venue"})
     public String Venue(Model model){
-        Venue venue = new Venue("de roma","https://www.google.be");
+        Venue venue = new Venue("de roma","https://denaalmoezenier.weebly.com");
         model.addAttribute("venue",venue);
+        venue.setCapacity(2000);
+        venue.setCity("antwerpen");
+        venue.setIndoor(true);
+        venue.setOutdoor(false);
+        venue.setFoodProvided(true);
+        venue.setDistanceFromPublicTransportInKm(1);
+        venue.setVenueName("Den Aalmoezenier");
+        venue.setLinkMoreInfo("https://denaalmoezenier.weebly.com");
         return "venuedetails";
     }
 }
--- src/main/java/be/thomasmore/party/model/Venue.java
+++ src/main/java/be/thomasmore/party/model/Venue.java
@@ -1,12 +1,28 @@
 package be.thomasmore.party.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Venue {
+    @Id
+    private Integer id;
     private String venueName;
     private String linkMoreInfo;
-
+    private int capacity;
+    private boolean foodProvided;
+    private boolean indoor;
+    private boolean outdoor;
+    private boolean freeParkingAvailable;
+    private String city;
+    private double distanceFromPublicTransportInKm;
     public Venue(String venueName, String linkMoreInfo) {
         this.venueName = venueName;
         this.linkMoreInfo = linkMoreInfo;
+    }
+
+    public Venue() {
+
     }
 
     public String getVenueName() {
@@ -23,5 +39,69 @@ public class Venue {
 
     public void setLinkMoreInfo(String linkMoreInfo) {
         this.linkMoreInfo = linkMoreInfo;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public boolean isFoodProvided() {
+        return foodProvided;
+    }
+
+    public boolean isIndoor() {
+        return indoor;
+    }
+
+    public boolean isOutdoor() {
+        return outdoor;
+    }
+
+    public boolean isFreeParkingAvailable() {
+        return freeParkingAvailable;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public double getDistanceFromPublicTransportInKm() {
+        return distanceFromPublicTransportInKm;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setFoodProvided(boolean foodProvided) {
+        this.foodProvided = foodProvided;
+    }
+
+    public void setIndoor(boolean indoor) {
+        this.indoor = indoor;
+    }
+
+    public void setOutdoor(boolean outdoor) {
+        this.outdoor = outdoor;
+    }
+
+    public void setFreeParkingAvailable(boolean freeParkingAvailable) {
+        this.freeParkingAvailable = freeParkingAvailable;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setDistanceFromPublicTransportInKm(double distanceFromPublicTransportInKm) {
+        this.distanceFromPublicTransportInKm = distanceFromPublicTransportInKm;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 }
